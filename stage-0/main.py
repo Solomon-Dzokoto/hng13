b9769354d028ce1e8d86b2f28f420650229f5175
Solomon-Dzokoto <dzokotosolomon85@gmail.com>
2025-10-16 11:16:30 +0000

    change the port

--- stage-0/main.py
+++ stage-0/main.py
@@ -76,4 +76,4 @@ if __name__ == "__main__":
 
     import uvicorn
 
-    uvicorn.run("main:app", host="127.0.0.1", port=8080, log_level="info")
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, log_level="info")
