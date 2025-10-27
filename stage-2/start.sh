78557d0a82ec99535dbff19d663c1163d674011d
Solomon-Dzokoto <dzokotosolomon85@gmail.com>
2025-10-27 17:38:57 +0000

    change the dir name

--- stage-2/start.sh
+++ stage-2/start.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 : "${PORT:=8080}"
 
 echo "Starting app on 0.0.0.0:${PORT}"
-exec python -m uvicorn app:app --host 0.0.0.0 --port "${PORT}"
+exec python -m uvicorn main:app --host 0.0.0.0 --port "${PORT}"
